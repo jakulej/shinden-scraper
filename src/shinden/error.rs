cd639698ac665d2e809d73ae5af4128f837c2eec
jakulej <j.kulej01@gmail.com>
2025-11-20 19:00:37 +0100

    Create custom error

---
+++ src/shinden/error.rs
@@ -0,0 +1,16 @@
+use std::fmt;
+
+#[derive(Debug, Clone)]
+pub enum ShindenError {
+    HtmlParsing,
+    WrongInput,
+}
+
+impl fmt::Display for ShindenError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+       match *self {
+           ShindenError::HtmlParsing => write!(f, "Problem ocured during html parsing"),
+           ShindenError::WrongInput => write!(f, "Invalid input"),
+       }
+    }
+}
