6cfdb2a50fd8f01af84de2518e087a962e6cb0c4
jakulej <j.kulej01@gmail.com>
2025-12-01 23:12:44 +0100

    Fixed player

--- src/shinden/player.rs
+++ src/shinden/player.rs
@@ -1,14 +1,15 @@
 use reqwest::blocking::Client;
 
-use super::ShindenError;
+use super::{ShindenClient, ShindenError};
 
-struct Player{
+pub struct Player{
     url: String,
     name: String
 }
 
 impl Player {
-    pub fn from_url(url: &str, clinet: Client) -> Result<Self,ShindenError>{
+    pub fn from_url(url: &str, client: &ShindenClient) -> Result<Self,ShindenError>{
         !todo!();
     }
 }
+
